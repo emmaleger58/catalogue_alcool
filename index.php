40b83e4faf33e6fd1bf3f09c10170a5e4abd8130
Emma Leger <e.leger@codeur.online>
2020-08-17 11:13:14 +0200

    admin

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
+<?php
+  // Initialiser la session
+  session_start();
+  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+  if(!isset($_SESSION["user"])){
+    header("Location: login.php");
+    exit();
+  }
+?>
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
+<html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Yoann Abran, Kevin Nguma, Emma Léger">
     <title>Jeux vidéos</title>
+  <link rel="stylesheet" href="style.css" />
   </head>
   <body>
-
+    <div class="sucess">
+    <h1>Bienvenue <?php echo $_SESSION['user']; ?>!</h1>
+    <p>C'est votre tableau de bord.</p>
+    <a href="logout.php">Déconnexion</a>
+    </div>
   </body>
 </html>
