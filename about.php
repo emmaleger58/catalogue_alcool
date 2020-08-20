a8b3189dbb151d8b1a70c762dfba41b13effbd89
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-08-20 14:35:28 +0200

    ok

---
+++ about.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+    <link rel="stylesheet" href="footer.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rochester&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+  </head>
+
+
+  <body>
+    <div id='container'>
+    <header>
+      <h1 id='titre'>À propos</h1>
+      <div id='box1'>
+        <div id='portrait'>
+          <img src="imgvid/moine.jpg">
+        </div>
+        <div id='biographie'>
+          <div id='vidcontainer'>
+            <video autoplay muted loop id="myVideo2">
+              <source src="imgvid/champs.mp4" type="video/mp4">
+            </video>
+            <div id='textcontainer'>
+              <p>bla bla bla</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </header>
+
+<!--À partir d'ici seulement footer-->
+
+    <footer>
+      <!-- Footer -->
+  <footer class="page-footer font-small blue pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mt-md-0 mt-3">
+
+          <!-- Content -->
+          <h5 class="col-md-3 text-uppercase">Contacte</h5>
+
+          <div id='info'>
+          <ul>
+          <li><div id='address'><i class="fas fa-map-marker-alt"></i><p>Germany, Francfort</p></div></li>
+
+          <li><div id='phone'><i class="fas fa-tty"></i><p>+214 85 662 43</p></div></li>
+
+          <li><div id='mail'><i class="fas fa-envelope"></i><p>brandname@mail.com</p></div></li>
+
+        </div>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mb-md-0 mb-3">
+
+          <!-- Links -->
+          <h5 class="text-uppercase">Mentions légales</h5>
+        <div id='info2'>
+          <p class="text-left">L'abus d'alcool est dangereux pour la santé, à consommer avec modération</p>
+        </div>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+          <!-- Links -->
+          <h5 class="text-uppercase">LOGO</h5>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="liens vers github">brandname.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </div>
+  </body>
+</html>
