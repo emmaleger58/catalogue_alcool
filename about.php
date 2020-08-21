a65831fc7fce4a6224211af8e581ace59ac5d213
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-08-21 15:26:54 +0200

    update

--- about.php
+++ about.php
@@ -10,21 +10,26 @@ include "include/config.php";
 </head>
   <body>
     <div id='container'>
+      <video autoplay muted loop id="myVideo">
+        <source src="imgvid/champs.mp4" type="video/mp4">
+      </video>
       <h1 id='titre'>À propos</h1>
       <div id='box1'>
         <div id='portrait'>
-          <img src="imgvid/moine.jpg">
+          <img class ="img-fluid. max-width:100%" src="imgvid/moine.jpg">
         </div>
         <div id='biographie'>
-          <div id='vidcontainer'>
-            <video autoplay muted loop id="myVideo2">
-              <source src="imgvid/champs.mp4" type="video/mp4">
-            </video>
-            <div id='textcontainer'>
-              <p>bla bla blajjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjbkbkbkbkbkbkkbkb</p>
+          <div class="card">
+            <div class="card-body">
+              <h2 id='title2'>"Notre Bière est la votre !"</h2>
+              <p>This is some text within a card body.</p>
+              <p>And i'm trying to make sure this card is responsive.</p>
+            </div>
+           </div>
           </div>
         </div>
       </div>
     </div>
+    <script src="main.js"></script>
 <!-- footer.php -->
 <?php include "include/footer.php"?>
