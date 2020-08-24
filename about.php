6c84940baccbf05a21f7f1fa3890d4a3344bbea7
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-08-24 14:41:33 +0200

    simple push

--- about.php
+++ about.php
@@ -12,14 +12,16 @@ include "include/config.php";
   <body>
     <div id='container'>
       <video autoplay muted loop id="myVideo">
-        <source src="imgvid/champs.mp4" type="video/mp4">
+        <source id=vidlist src="imgvid/champs.mp4" type="video/mp4">
+        <!-- <source src="imgvid/Hops.mp4" type="video/mp4">
+        <source src="imgid/Drink.mp4" type="video/mp4">   -->
       </video>
       <h1 id='titre'>À propos</h1>
-      <div id='box1'>
-        <div id='portrait'>
+      <div  class="d-flex row justify-content-center" id='box1'>
+        <div class="d-flex col-lg-6 col-12 justify-content-center" id='portrait'>
           <img class ="img-fluid. max-width:100%" src="imgvid/moine.jpg">
         </div>
-        <div id='biographie'>
+        <div class="d-flex flex-column align-items-center col-lg-6 col-12"id='biographie'>
           <div class="card">
             <div class="card-body">
               <h2 id='title2'>"Notre Bière est la votre !"</h2>
@@ -31,7 +33,7 @@ include "include/config.php";
               Cependant, Lucien P, Evêque de la commune de Dontranc à cette époque, s'oppose fortement au moine bienfaiteur.
               C'est alors que Theobald P montre son ingéniosité , il décide de vendre son breuvage afin d'en faire partager les profits au Diocèse Dontrainc et aux plus pauvres.
               C'est alors qu'il se voit autorisé à poursuivre sa missions.</p>
-              <p>"Les bière du Theobald, rebaptisé la "La Cave Key" en 1950 compte aujourd'hui plusieurs recettes de bières, de partenaires, mais aussi de marques.
+              <p>"Les bières du Theobald", rebaptisé la "La Cave Key" en 1950 compte aujourd'hui plusieurs recettes de bières, de partenaires, mais aussi de marques.
               Cependant ses objectifs n'ont pas changer, partager "l'amour de la pinte" et étancher la soif de son prochain.
               C'est pourquoi La Cave Key offres aujourd'hui 60% de ses revenues à des oeuvres caritatives.
               La Cave Key; c'est une tradition, une histoire, un partage. </p>
@@ -40,7 +42,6 @@ include "include/config.php";
           </div>
         </div>
       </div>
-    </div>
     <script src="main.js"></script>
 <!-- footer.php -->
 <?php include "include/footer.php"?>
