fee1f8f9e2b05668df0d16149bb4ea6e4ccb166c
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-08-25 16:55:46 +0200

    something to finish!

--- main.js
+++ main.js
@@ -1,37 +1,23 @@
 var image=document.getElementById('portrait');
 var text=document.getElementById('biographie');
+var bulle=document.getElementById('bulle');
 
 image.addEventListener("mouseenter", function( event ) {
-  biographie.style.opacity="1";})
+  biographie.style.opacity="1",
+  bulle.style.opacity="0";})
 
 image.addEventListener("mouseleave" , function( event ) {
-  biographie.style.opacity="0";})
+  biographie.style.opacity="0",
+  bulle.style.opacity="1"})
 
 
 image.addEventListener("mouseenter", function( event ) {
-  biographie.style.transform="scale(1)";})
+  biographie.style.transform="scale(1)",
+  bulle.style.transform="scale(0)"})
 
 image.addEventListener("mouseleave", function( event ) {
-  biographie.style.transform="scale(0)";})
+  biographie.style.transform="scale(0)",
+  bulle.style.transform="scale(1)"})
 
 
-//background video playlist
-
-
-// var video = document.getElementById('myVideo');
-// var source = document.createElement('source');
-//
-// source.setAttribute('src', 'https://static.videezy.com/system/resources/previews/000/032/846/original/girllistensong1.mp4');
-//
-// video.appendChild(source);
-// video.play();
-//
-// 
-// setTimeout(function() {
-//     video.pause();
-//
-//     source.setAttribute('src', 'https://static.videezy.com/system/resources/previews/000/012/979/original/record_player_011.mp4');
-//
-//     video.load();
-//     video.play();
-// }, 14000);
+//Bulle
